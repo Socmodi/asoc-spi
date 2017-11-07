3cc98365c083e930b44675bf1d6697cdff3c096d
Socmodi <www.dhjune@163.com>
2017-11-07 15:19:24 +0800

    update

---
+++ asoc-spi-core/src/main/java/org/asocframework/spi/model/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author jiqing
+ * @version $Id: package-info，v 1.0 2017/11/7 14:22 jiqing Exp $
+ * @desc
+ */
+package org.asocframework.spi.model;
---
+++ asoc-spi-core/src/main/java/org/asocframework/spi/model/partner/Partner.java
@@ -0,0 +1,9 @@
+package org.asocframework.spi.model.partner;
+
+/**
+ * @author jiqing
+ * @version $Id: Partner，v 1.0 2017/11/7 14:27 jiqing Exp $
+ * @desc
+ */
+public class Partner {
+}
